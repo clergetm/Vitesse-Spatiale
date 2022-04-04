f4f1d08479b8e68f6e670b9336b40c0457e1994e
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2022-04-04 09:06:43 -0400

    Initialisation Repo

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+* Lilou		ALLAIN	ALLL26520207
+* Mathys	CLERGET	CLEM14050107
+* Guillaume URVOY	URVG07119904
+*/
+
+int main() {
+	setlocale(LC_CTYPE, "fr_FR");
+
+	cout << "Hello, Groupe 5" << endl;
+
+	system("pause");
+	return 0;
+}
