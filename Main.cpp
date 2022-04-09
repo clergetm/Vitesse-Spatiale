22eb165fd6f5a811a6ff44587cc6a698ecaf0900
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2022-04-09 14:08:54 -0400

    Structure liste en Tableau

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "liste.h"
 using namespace std;
 
 /*
@@ -8,10 +9,55 @@ using namespace std;
 * Guillaume URVOY	URVG07119904
 */
 
+void test_Liste() {
+	cout << "/////test_Liste///////////////////////" << '\n';
+	Liste<int> l = Liste<int>(4);
+
+	l.insererFin(1);
+	l.insererFin(2);
+	l.insererFin(3);
+	l.insererFin(4);
+
+	cout << "Affichage liste INT" << '\n';
+	string res;
+	for (l.fixerTete(); l.estDansListe(); l.suivant()) {
+		int _temp = l.valeurCourante();
+		res += to_string(_temp)+ '\n';
+	}
+	cout << res << '\n';
+
+	cout << "Trouver" << '\n';
+	cout << "Trouver 1 (doit être true)  : " << l.trouver(1) << '\n';
+	cout << "  - Valeur Courante  : " << l.valeurCourante() << '\n';
+
+	cout << "Trouver 5 (doit être false) : " << l.trouver(5) << '\n';
+	cout << "  - Valeur Courante  : " << l.valeurCourante() << '\n';
+
+	cout << "Longueur : " << to_string(l.longueur()) << '\n';
+	cout << "Suppression" << '\n';
+	cout << "- suppression 4 " << '\n';
+	l.supprimer(4);
+	cout << "- suppression 2" << '\n';
+	l.supprimer(2);
+	cout << "- suppression 5" << '\n';
+	l.supprimer(5);
+
+	cout << "Longueur : " << to_string(l.longueur()) << '\n';
+
+	cout << "- Affichage liste INT" << '\n';
+	res = "";
+	for (l.fixerTete(); l.estDansListe(); l.suivant()) {
+		int _temp = l.valeurCourante();
+		res += " - "+to_string(_temp) + '\n';
+	}
+	cout << res << endl;
+
+}
+
 int main() {
 	setlocale(LC_CTYPE, "fr_FR");
 
-	cout << "Hello, Groupe 5" << endl;
+	test_Liste();
 
 	system("pause");
 	return 0;
--- liste.h
+++ liste.h
@@ -3,24 +3,156 @@
 #include <string>
 using namespace std;
 
+/**
+* Implémentation de la liste sous forme de tableau.
+* 
+* Dans le cadre de ce projet, le nombre d’éléments tels que les planètes, les vaisseaux reste inchangé dès l’initialisation de la liste.
+* Le projet ne nécessite pas l’ajout ou la suppression d’éléments.
+*/
 template <class T>
-class liste
+class Liste
 {
-	noeud<T>* tete;				// Pointeur de la tête de la liste.
-	noeud<T>* courant;			// Pointeur de l'élément courant de la liste.
-	noeud<T>* queue;			// Pointeur de la queue de la liste.
-
-
-	liste();					// Constructeur de la liste.
-	~liste();					// Destructeur de la liste.
+private:
+	short int courant;			// Index courant.
+	short int nbElement;		// Nombre d’élément dans la liste.
+	short int tailleMax;		// Taille maximale de la liste.
+	T* elements;				// Tableau d’éléments.
+public:
+	Liste(const short int);		// Constructeur de la liste.
+	~Liste();					// Destructeur de la liste.
 	bool trouver(const T&);		// Trouver un élément dans la liste.
 	void supprimer(const T&);	// Supprimer un élément dans la liste.
 	void insererFin(const T&);	// Insérer un élément à la liste.
 	void fixerTete();			// Fixer le noeud courant à la tête de la liste.
 	bool estDansListe();		// Vérifier que le noeud courant est dans la liste.
-	void suivant();				// Déplacer le pointeur courant sur le noeud suivant.
+	void suivant();				// Incrémenter de 1 le compteur courant.
 	T valeurCourante() const;	// Obtenir la valeur courante.
 	string toString();			// Obtenir une représentation textuelle des éléments de la liste.
 	short int longueur() const;	// Obtenir le nombre d’élément dans la liste.
 };
+
+//// CONSTRUCTEURS DESTRUCTEURS ///////////////////////////////////////////////////////////////////
+
+/**
+* Constructeur de liste.
+* @param	_taille : taille maximale du tableau d’éléments.
+*/
+template<class T>
+Liste<T>::Liste(const short int _taille) {
+	this->tailleMax = _taille;
+	nbElement = 0;
+	courant = 0;
+	elements = new T[_taille];
+}
+
+/**
+* Destructeur de liste.
+*/
+template<class T>
+Liste<T>::~Liste() {
+	delete[] elements;
+}
+
+//// AUTRES FONCTIONS /////////////////////////////////////////////////////////////////////////////
+
+
+/**
+* Fonction indiquant la présence, ou non, d’un élément dans la liste.
+* @param	_element : l’élément à trouver.
+* @returns	true si l’élément existe dans la liste, false sinon.
+*/
+template<class T>
+bool Liste<T>::trouver(const T& _element) {
+	for (fixerTete(); estDansListe(); suivant()) {
+		if (valeurCourante() == _element) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+* Supprimer l’élément voulu de la liste, s’il est dans la liste.
+* @param	_element : l’élément à chercher et supprimer.
+*/
+template<class T>
+void Liste<T>::supprimer(const T& _element) {
+	// Si l’élément est dans le tableau
+	if (trouver(_element)) {
+		// On décale tous les éléments depuis son emplacement
+		for (short int i = courant; i < nbElement - 1; i++) {
+			elements[i] = elements[i + 1];
+		}
+		nbElement--;
+	}
+}
+
+/**
+* Ajouter l’élément à la fin de la liste.
+* @param	_element : l’élément à ajouter à la liste.
+*/
+template<class T>
+void Liste<T>::insererFin(const T& _element) {
+	elements[nbElement] = _element;
+	nbElement++;
+}
+
+/**
+* Repositionner le pointeur courant à la tête de la liste.
+*/
+template <class T>
+void Liste<T>::fixerTete() {
+	courant = 0;
+}
+
+
+/**
+* Vérifier que le pointeur courant se trouve dans la liste.
+* @return	true si le pointeur ne se trouve pas en fin de liste, false sinon.
+*/
+template <class T>
+bool Liste<T>::estDansListe() {
+	return (courant >= 0) && (courant < nbElement);
+}
+
+/**
+* Incrémenter le compteur courant.
+*/
+template <class T>
+void Liste<T>::suivant() {
+	courant++;
+}
+
+/**
+* Obtenir l’élément courant dans la liste.
+* @return	un élement de type T.
+*/
+template <class T>
+T Liste<T>::valeurCourante() const {
+	return elements[courant];
+}
+
+/**
+* Obtenir une représentation textuelle des éléments de la liste.
+* @return	un string avec la représentation textuelle de chaque élément de la liste.
+*/
+template <class T>
+string Liste<T>::toString() {
+	string res;
+	for (fixerTete(); estDansListe(); suivant()) {
+		T _temp = valeurCourante();
+		res += _temp.toString() + '\n';
+	}
+	return res;
+}
+
+/**
+* Obtenir le nombre d’élément dans la liste.
+* @return	un short int du nombre d’élément.
+*/
+template <class T>
+short int Liste<T>::longueur() const {
+	return nbElement;
+}
 
